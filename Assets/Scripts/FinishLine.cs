f7b50662f248d65f95aff6fb3bac23d1b06d2ff0
TungNT140601 <tungnt14062001@gmail.com>
2023-06-21 15:07:19 +0700

    add source code

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+
+    [SerializeField] float delayTime = 2f;
+    [SerializeField] ParticleSystem finishEffect;
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player")
+        {
+            Debug.Log("You Finished!");
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", delayTime); // Invoke has to use a method that you are delaying
+        }
+    }
+
+    void ReloadScene() //Created reload scene method in order to use invoke
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
